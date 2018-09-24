d8e88bf1dbea0cb027b589b537ff87cd86ef7e85
Nikola Marjanovic <nikolamarjanovic@Nikolas-MacBook-Pro.local>
2018-09-24 23:08:48 +0200

    refactoring code

--- src/Framework/Game.ts
+++ src/Framework/Game.ts
@@ -1,25 +1,24 @@
 import Scene from './Scene';
+import Entity from './Entity';
 
 class Game {
-  fps: number;
-  showFps: false;
+  fps: number = 60;
+  fpsLimit: number;
+  showFps: boolean = false;
   previousFrameTime: number;
   scene: any;
-  entities: any[];
+  entities: Entity[] = [];
 
-  constructor({width = 400, height = 400, entities = [], fps = 15, showFps = false} : any) {
+  constructor({width = 400, height = 400} : any) {
     this.run = this.run.bind(this);
     this.gameLoop = this.gameLoop.bind(this);
     this.update = this.update.bind(this);
     this.draw = this.draw.bind(this);
 
-    this.fps = fps;
-    this.showFps = showFps;
     this.scene = new Scene({width, height});
-    this.entities = entities;
   }
 
-  createGame({width = 400, height = 400, fps = 15, showFps = false} : any) {
+  createGame({width = 400, height = 400, fps = 60, showFps = false} : any) {
     this.fps = fps;
     this.showFps = showFps;
     this.scene = new Scene({width, height});
@@ -30,9 +29,9 @@ class Game {
     requestAnimationFrame(this.gameLoop);
   }
 
-  private gameLoop(time: number) {
+  private gameLoop(timestamp: number) {
     this.update();
-    this.draw(time);
+    this.draw(timestamp);
     requestAnimationFrame(this.gameLoop);
   }
 
@@ -42,17 +41,17 @@ class Game {
     }
   }
 
-  private draw(time: number) {
+  private draw(timestamp: number) {
     this.scene.clear();
-    this.drawFps(time);
+    this.drawFps(timestamp);
     for (let entity of this.entities) {
       entity.draw(this.scene);
     }
-    this.previousFrameTime = time;
+    this.previousFrameTime = timestamp;
   }
 
-  private drawFps(time: number) {
-    const fps = Math.floor(1000 / (time - this.previousFrameTime));
+  private drawFps(timestamp: number) {
+    const fps = Math.floor(1000 / (timestamp - this.previousFrameTime));
     this.scene.context.fillStyle = 'white';
     this.scene.context.fillText(fps, this.scene.canvas.width - 17, 10);
   }
